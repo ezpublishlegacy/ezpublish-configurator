439d95d004b2c2e1e65e3b22404c02dd23056661
Martin Eckhardt <martin.eckhardt@prosiebensat1digital.de>
2014-04-03 13:57:58 +0200

    Added logging capability to config updater
    * Introduced monolog/monolog.
    * Added a log-file option, default is PHP STDOUT stream.
    * Some CS.
    * Warning if file is not found instead of exception.

--- src/Psd/Ezpublish/Configurator/Configurator.php
+++ src/Psd/Ezpublish/Configurator/Configurator.php
@@ -2,6 +2,8 @@
 
 namespace Psd\Ezpublish\Configurator;
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
@@ -9,7 +11,13 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Yaml;
 
-class Configurator {
+class Configurator
+{
+
+    /**
+     * @var Logger
+     */
+    private $logger = null;
 
     public function __construct()
     {
@@ -20,7 +28,14 @@ class Configurator {
             ->setDefinition(
                 array(
                     new InputArgument('configuration-file', InputArgument::REQUIRED, 'The configuration file.'),
-                    new InputArgument('ezpublish-root', InputArgument::REQUIRED, 'The eZ Publish root directory.')
+                    new InputArgument('ezpublish-root', InputArgument::REQUIRED, 'The eZ Publish root directory.'),
+                    new InputOption(
+                        'log-file',
+                        null,
+                        InputOption::VALUE_REQUIRED,
+                        'A stream to log to. Default STDOUT',
+                        'php://stdout'
+                    )
                 )
             )
             ->setDescription('Updates eZ publish configuration.')
@@ -35,8 +50,8 @@ class Configurator {
 
         $root_dir = $input->getArgument('ezpublish-root');
 
-        $file = $input->getArgument('configuration-file');
-        $config = file_get_contents($file);
+        $ymlFile = $input->getArgument('configuration-file');
+        $config = file_get_contents($ymlFile);
         $directories = Yaml::parse($config);
 
         // Change dir to ezpublish root.
@@ -50,7 +65,11 @@ class Configurator {
                 // Check if files are available.
                 $file = sprintf('%s/%s', $directory, $filename);
                 if (file_exists($file) === false) {
-                    throw new \Exception(sprintf('File not found "%s", cannot overwrite block values in ini file.', $file));
+                    $this->logger->log(
+                        Logger::ERROR,
+                        sprintf('File not found "%s", cannot overwrite block values in ini file.', $file)
+                    );
+                    continue;
                 }
 
                 $ini = \eZINI::fetchFromFile($file);
@@ -67,6 +86,10 @@ class Configurator {
                     $ini->BlockValues[$block] = $blockValues;
                 }
                 $ini->save(realpath($file), false, false, false, true, false, true);
+                $this->logger->log(
+                    Logger::INFO,
+                    "Updated ezpublish INI \"{$file}\" with settings from {$ymlFile}"
+                );
             }
         }
 
@@ -76,12 +99,24 @@ class Configurator {
 
     protected function validateInput(InputInterface $input, OutputInterface $output)
     {
+        $this->logger = new Logger(
+            'logger',
+            array(new StreamHandler($input->getOption('log-file')))
+        );
+
         $root_dir      = $input->getArgument('ezpublish-root');
+        $configFile    = $input->getArgument('configuration-file');
         $autoload_file = $root_dir.'/'.'autoload.php';
+
+        if (realpath($configFile) === false) {
+            $msg = sprintf('Could not find configuration file "%s".', $configFile);
+            $this->logger->log(Logger::EMERGENCY, $msg);
+            exit(1);
+        }
 
         if (is_dir(realpath($root_dir)) === false) {
             $msg = sprintf('Could not find eZPublish root directory "%s".', realpath($root_dir));
-            $output->writeln('<error>'.$msg.'</error>');
+            $this->logger->log(Logger::EMERGENCY, $msg);
             exit(1);
         }
 
@@ -90,16 +125,11 @@ class Configurator {
                 'Could not find autoload.php file. Probably you do not point to a eZ Publish root directory.',
                 $root_dir
             );
-            $output->writeln('<error>'.$msg.'</error>');
+            $this->logger->log(Logger::EMERGENCY, $msg);
             exit(1);
         }
 
         include $autoload_file;
-    }
-
-    protected function getFilename()
-    {
-
     }
 
     protected function getErrorText($prolog)
@@ -147,6 +177,4 @@ extension/mydesign/settings:
 EOF;
         return $help;
     }
-
 }
-
